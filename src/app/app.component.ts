e758f263634c4479e0e1e9bedfa39534d538b784
prafful <prraful@gmail.com>
2018-07-19 11:22:13 +0530

    ngrs store and angular example with todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,39 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {todoServiceService} from './services/todo-service.service'
 import {Todo} from './todo/todo'
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+@Component({selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css']})
 export class AppComponent {
   title = 'app';
-  newtodo:Todo = new Todo()
+  newtodo : Todo = new Todo()
 
-  constructor(private todoservice:todoServiceService){}
-  
-  addTodoLocal = function(){
+  constructor(private todoservice : todoServiceService) {}
+
+  addTodoLocal = function () {
     console.log(this.newtodo)
     this.newtodo = new Todo()
     //this.todo = ""
-    this.todoservice.addTodo(this.newtodo)
-    
-   
+    this
+      .todoservice
+      .addTodo(this.newtodo)
+  }
+
+   public get pendingtodo() : Array < Todo > {
+
+    //console.log("PENDING TODO");
+    //console.log(this.todoservice.getPendingTodo());  
+
+    return this
+      .todoservice
+      .getPendingTodo()
+  } 
+
+  public toggletodo(eachtodo): void{
+    this.todoservice.toggleTodo(eachtodo)
+  }
+
+  public get completetodo():Array<Todo>{
+    return this.todoservice.getCompleteTodo()
   }
 
 }
--- src/app/services/todo-service.service.ts
+++ src/app/services/todo-service.service.ts
@@ -18,8 +18,47 @@ export class todoServiceService {
     this.alltodos.push(todo)
     console.log("from service - addtodo")
     console.log(this.alltodos)
+    console.log("hello");
+    /* //console.log(this.alltodos.filter((onetodo) => {
+                                                    onetodo.status === false
+                                                    return onetodo
+                                                  })); */
     return this
   }
 
+  public getPendingTodo():Todo[]{
+    //console.log("hello");
+    //console.log(this.alltodos.filter((onetodo) => {onetodo.status == false}));
+    return this.alltodos.filter((onetodo) => {
+                                              onetodo.status === false
+                                              return onetodo
+                                            })
+  }
+
+  public toggleTodo(todo:Todo): void{
+    let updatedtodo = this.updatetodobyid(todo.id, {
+                                                      status:!todo.status
+                                                    })
+  }
+
+  private updatetodobyid(id: number, values:Object ={}){
+
+    let todo = this.gettodobyid(id)
+    if(!todo){
+      return null
+    }
+
+  }
+
+  private gettodobyid(id:number):Todo{
+    return this.alltodos.filter(todo => todo.id === id).pop()
+  }
+
+  public getCompleteTodo(): Todo[]{
+    return this.alltodos.filter((onetodo) => {
+                                                onetodo.status === true
+                                                return onetodo
+                                              })
+  }
 
 }
